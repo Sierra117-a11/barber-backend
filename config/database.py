36fab20df7386d2dadce26471712b9e9291cbcb1
julian_117 <venegasjulian04@gmail.com>
2025-06-06 20:46:54 -0500

    Update database.py

--- config/database.py
+++ config/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # Configuración de la base de datos SIN CONTRASEÑA
 #DATABASE_URL = "mysql+pymysql://root@localhost/python" 
  
-DATABASE_URL = "mysql://root:HjAifvyKXDoKXamMpiAcVNhcSzFsBMCF@maglev.proxy.rlwy.net:54052/railway"  
+DATABASE_URL = "mysql+pymysql://root:HjAifvyKXDoKXamMpiAcVNhcSzFsBMCF@maglev.proxy.rlwy.net:54052/railway"  
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
